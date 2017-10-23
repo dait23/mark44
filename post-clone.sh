18fcfe46340e361d56d38255d129fa14058543ec
Aditya Maulana <dait23@gmail.com>
2017-10-23 07:47:45 +0700

    Create post-clone.sh

---
+++ post-clone.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Example: Clone a required repository
+git clone https://github.com/example/SomeProject
+
+# Example: Install Mobile Center CLI
+npm install -g mobile-center-cli
